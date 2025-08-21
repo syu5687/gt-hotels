01272a38e0a3d2e734b98415a5874bafb29e4728
syu5687 <mk@emanet.jp>
2025-08-21 14:36:48 +0900

    qr

--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,5 @@
 <?php require_once __DIR__.'/../config/app.php'; ?>
+<img src="<?= asset('/_assets/images/gth-qr.png') ?>" alt="qr">
 <footer class="site-footer">
   <div class="footer-inner">
 	<small>© <?= date('Y') ?> Garden Terrace Hotels</small>
