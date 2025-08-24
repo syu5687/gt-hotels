ca998eaa386240fa4e8409d4902dd2a3f585410a
syu5687 <mk@emanet.jp>
2025-08-25 07:49:16 +0900

    Update head.php

--- partials/head.php
+++ partials/head.php
@@ -6,8 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
   <title><?= isset($pageTitle)? ($pageTitle.' | '):'' ?>Garden Terrace Hotels</title>
   <meta name="description" content="<?= isset($pageDesc)? htmlspecialchars($pageDesc) : 'Garden Terrace Hotels | Luxury / City Resort / Relax' ?>" />
-
-  <link rel="preload" as="image" href="<?= asset('/_assets/images/hero.jpg') ?>">
   <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&display=swap" rel="stylesheet">
 
   <link rel="stylesheet" href="<?= asset('/_assets/css/style.css') ?>">
