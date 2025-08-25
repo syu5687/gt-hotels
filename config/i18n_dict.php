a73f8d2c907c9ee82026dac3a891ff20499668da
syu5687 <mk@emanet.jp>
2025-08-25 11:28:02 +0900

    Update i18n_dict.php

--- config/i18n_dict.php
+++ config/i18n_dict.php
@@ -5,14 +5,29 @@ return [
   '*' => [
     // 共通で守りたい固有名詞など
     'GARDEN TERRACE NAGASAKI' => 'GARDEN TERRACE NAGASAKI',
+    'GARDEN TERRACE NAGASAKI' => 'GARDEN TERRACE SAGA',
+    'GARDEN TERRACE NAGASAKI' => 'GARDEN TERRACE FUKUOKA',
+    'URARI TAKEO GARDEN TERRACE SPA RESORTS' => 'URARI TAKEO GARDEN TERRACE SPA RESORTS',
+    'ROYAL CHESTER NAGASAKI HOTEL & RETREAT' => 'ROYAL CHESTER NAGASAKI HOTEL & RETREAT',
+    'HOTEL FLAGS SASEBO KUJUKUSHIMA' => 'HOTEL FLAGS SASEBO KUJUKUSHIMA',
+    'HOTEL FLAGS ISAHAYA NAGASAKI' => 'HOTEL FLAGS ISAHAYA NAGASAKI',
+    'HOTEL FLAGS SASEBO KUJUKUSHIMA the BEKKAN' => 'HOTEL FLAGS SASEBO KUJUKUSHIMA the BEKKAN',
+    'GOTO KON-KANA KINGDOM WINERY & RESORT' => 'GOTO KON-KANA KINGDOM WINERY & RESORT',
   ],
   'en' => [
     'レストラン「フォレスト」' => 'Restaurant “FOREST”',
-    'フォレスト' => 'FOREST',
+    '鉄板焼ダイニング「竹彩」' => 'Teppanyaki dining "CHIKUSAI"',
+    '鮨ダイニング「天空」'=> 'Sushi dining "TENKU"',
+    '懐石料理と天ぷら「秋月」'=> 'Kaiseki Cuisine and Tempura "AKIZUKI"',
+    '九州創作「千山万水」' => 'Kyusyu Creative "Senzan Bansui"',
   ],
   'tc' => [
     'レストラン「フォレスト」' => '餐廳「FOREST」',
     'フォレスト' => 'FOREST',
+    '竹彩' => 'CHIKUSAI',
+    '天空' => 'TENKU',
+    '秋月' => 'AKIZUKI',
+    '千山万水' => 'SENZAN BANSUI',
   ],
   'sc' => [
     'レストラン「フォレスト」' => '餐厅“FOREST”',
