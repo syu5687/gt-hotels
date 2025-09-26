3d6e1695c63f2de12a43e36e6812e49b56ef4781
01emastaff <matsufuji@emanet.jp>
2025-09-26 16:40:45 +0900

    Update rcn.php

--- rcn.php
+++ rcn.php
@@ -255,7 +255,7 @@ require __DIR__ . '/partials/header.php';
 		  <a href="https://www.nagasaki-rch.jp/" target="_blank" rel="noopener" class="hpLink">https://www.nagasaki-rch.jp/</a></p><br>
 		  <span class="mark">■</span>
 			〒852-8156 長崎県長崎市赤迫3–6-10
-			<a href="https://maps.app.goo.gl/RGPvZBPcrP944V7c9" class="map" target="_blank" rel="noopener">(GoogleMap<img src="<?= asset('/_assets/images/window.svg') ?>" alt="別ウィンドウ" class="iconwindow" style="display: inline-block;">)</a>
+			<a href="https://maps.app.goo.gl/RGPvZBPcrP944V7c9" class="map" target="_blank" rel="noopener">(GoogleMAP<img src="<?= asset('/_assets/images/window.svg') ?>" alt="別ウィンドウ" class="iconwindow" style="display: inline-block;">)</a>
 	  </div>
 
 	</div>
