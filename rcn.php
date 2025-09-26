95ac92f83dfa9e10036f4607f34f415959773177
01emastaff <matsufuji@emanet.jp>
2025-09-26 16:25:16 +0900

    Update rcn.php

--- rcn.php
+++ rcn.php
@@ -251,8 +251,8 @@ require __DIR__ . '/partials/header.php';
 		<div class="spec-divider"></div>
 
 		<p class="spec-note">
-		  <span class="mark">■</span>公式ホームページ
-		  <a href="https://www.nagasaki-rch.jp/" target="_blank" rel="noopener">https://www.nagasaki-rch.jp/</a></p><br>
+		  <span class="mark">■</span>公式サイト
+		  <a href="https://www.nagasaki-rch.jp/" target="_blank" rel="noopener" class="hpLink">https://www.nagasaki-rch.jp/</a></p><br>
 		  <span class="mark">■</span>
 			〒852-8156 長崎県長崎市赤迫3–6-10
 			<a href="https://maps.app.goo.gl/RGPvZBPcrP944V7c9" class="map" target="_blank" rel="noopener">(GoogleMap<img src="<?= asset('/_assets/images/window.svg') ?>" alt="別ウィンドウ" class="iconwindow" style="display: inline-block;">)</a>
